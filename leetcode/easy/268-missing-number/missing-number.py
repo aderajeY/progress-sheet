a24a807c257cc91cb3e5723415ddf39343a3bc04
Aderajew Yeshiwendm <adeyeshi294@gmail.com>
2024-07-01 19:15:32 +0300

    Time: 105 ms (84.03%) | Memory: 17.7 MB (87.27%) - LeetSync

--- leetcode/easy/268-missing-number/missing-number.py
+++ leetcode/easy/268-missing-number/missing-number.py
@@ -1,11 +1,17 @@
 class Solution:
     def missingNumber(self, nums: List[int]) -> int:
-        nums.sort()
         n = len(nums)
-        for index in range(n):
-            if nums[index] != index:
-                return index
-        return n
-            
+        xor_all = 0
+
+        # XOR all the elements in nums
+        for num in nums:
+            xor_all ^= num
+
+        # XOR the result with all the numbers from 0 to n
+        for i in range(n + 1):
+            xor_all ^= i
+
+        return xor_all
+
             
         
