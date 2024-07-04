968bade4ca5fc3ad9492a9a3c42315ddab6d6bd0
Aderajew Yeshiwendm <adeyeshi294@gmail.com>
2024-07-04 18:01:23 +0300

    Time: 647 ms (5.00%) | Memory: 16.4 MB (55.65%) - LeetSync

--- leetcode/easy/1013-fibonacci-number/fibonacci-number.py
+++ leetcode/easy/1013-fibonacci-number/fibonacci-number.py
@@ -1,7 +1,9 @@
 class Solution:
     def fib(self, n: int) -> int:
+        memo = {}
         if n == 0 or n== 1:
             return n
-        else:
-            return self.fib(n-1) + self.fib(n-2)
+        if n not in memo:
+            memo[n] = self.fib(n-1) + self.fib(n-2)
+        return memo[n]
         
