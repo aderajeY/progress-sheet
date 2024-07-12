8a89596f0ad579a1c1eb17ffa98b7fd6daf0617f
Aderajew Yeshiwendm <adeyeshi294@gmail.com>
2024-07-12 12:09:19 +0300

    Time: 165 ms (43.37%) | Memory: 18 MB (63.43%) - LeetSync

---
+++ leetcode/easy/645-set-mismatch/set-mismatch.py
@@ -0,0 +1,17 @@
+class Solution:
+    def findErrorNums(self, nums: List[int]) -> List[int]:
+        n = len(nums)
+        pointer = 0
+        while pointer < n:
+            cur = nums[pointer]
+            if cur - 1 != pointer and cur != nums[cur -1]:
+                nums[pointer], nums[cur - 1] = nums[cur - 1], nums[pointer]
+            else:
+                pointer += 1
+        res = []
+        for j in range(n):
+            if nums[j] != j + 1:
+                res.append(nums[j])
+                res.append(j + 1)
+                
+        return res
