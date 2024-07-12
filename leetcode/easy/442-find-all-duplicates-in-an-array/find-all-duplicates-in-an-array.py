e5a9fb94941c10cdcbb5e49028af899e45b18560
Aderajew Yeshiwendm <adeyeshi294@gmail.com>
2024-07-12 12:07:08 +0300

    Time: 270 ms (39.14%) | Memory: 23.7 MB (90.71%) - LeetSync

---
+++ leetcode/easy/442-find-all-duplicates-in-an-array/find-all-duplicates-in-an-array.py
@@ -0,0 +1,16 @@
+class Solution:
+    def findDuplicates(self, nums: List[int]) -> List[int]:
+        n = len(nums)
+        pointer = 0
+        while pointer < n:
+            cur = nums[pointer]
+            if cur - 1 != pointer and cur != nums[cur -1]:
+                nums[pointer], nums[cur - 1] = nums[cur - 1], nums[pointer]
+            else:
+                pointer += 1
+        res = []
+        for j in range(n):
+            if nums[j] != j + 1:
+                res.append(nums[j])
+                
+        return res
