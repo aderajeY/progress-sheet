a71ae7ecba646b22f8b1f1d5889dd2e49c3c7c98
Aderajew Yeshiwendm <adeyeshi294@gmail.com>
2024-07-13 16:02:16 +0300

    Time: 116 ms (48.82%) | Memory: 18.2 MB (6.68%) - LeetSync

---
+++ leetcode/easy/690-employee-importance/employee-importance.py
@@ -0,0 +1,18 @@
+class Solution:
+    def getImportance(self, employees: List['Employee'], id: int) -> int:
+        graph = defaultdict(list)
+        vals = defaultdict(int)
+
+        for e in employees:
+            graph[e.id].extend(e.subordinates)
+            vals[e.id] = e.importance
+
+        def dfs(node):
+            res = 0
+
+            for nei in graph[node]:
+                res += vals[nei] + dfs(nei)
+        
+            return res
+
+        return vals[id] + dfs(id)
