16ff4294e99ebd3801c62b9e2425306ad887f1b6
Aderajew Yeshiwendm <adeyeshi294@gmail.com>
2024-07-14 08:55:30 +0300

    Time: 172 ms (95.85%) | Memory: 17.5 MB (30.25%) - LeetSync

--- leetcode/easy/547-number-of-provinces/number-of-provinces.py
+++ leetcode/easy/547-number-of-provinces/number-of-provinces.py
@@ -1,39 +1,31 @@
-from typing import List
-
-class UnionFind:
-    def __init__(self, size):
-        self.parent = list(range(size))
-        self.rank = [1] * size
-
-    def find(self, x):
-        if self.parent[x] != x:
-            self.parent[x] = self.find(self.parent[x])  # Path compression
-        return self.parent[x]
-
-    def union(self, x, y):
-        rootX = self.find(x)
-        rootY = self.find(y)
-
-        if rootX != rootY:
-            if self.rank[rootX] > self.rank[rootY]:
-                self.parent[rootY] = rootX
-            elif self.rank[rootX] < self.rank[rootY]:
-                self.parent[rootX] = rootY
-            else:
-                self.parent[rootY] = rootX
-                self.rank[rootX] += 1
-
 class Solution:
-    def findCircleNum(self, isConnected: List[List[int]]) -> int:
-        n = len(isConnected)
-        union_find = UnionFind(n)
-
+    def findCircleNum(self, M: List[List[int]]) -> int:
+        
+        graph = collections.defaultdict(list)
+        
+        if not M:
+            return 0
+        
+        n = len(M)
         for i in range(n):
-            for j in range(i + 1, n):
-                if isConnected[i][j] == 1:
-                    union_find.union(i, j)
-
-        provinces = len(set(union_find.find(i) for i in range(n)))
-        return provinces
-
-
+            for j in range(i+1,n):
+                if M[i][j]==1:
+                    graph[i].append(j)
+                    graph[j].append(i)
+        
+        visit = [False]*n
+        
+        def dfs(u):
+            for v in graph[u]:
+                if visit[v] == False:
+                    visit[v] = True
+                    dfs(v)
+        
+        count = 0
+        for i in range(n):
+            if visit[i] == False:
+                count += 1
+                visit[i] = True
+                dfs(i)
+        
+        return count
