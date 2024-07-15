089128375a7d7b4e2ed2ab97c3c9415dce6ac9cf
Aderajew Yeshiwendm <adeyeshi294@gmail.com>
2024-07-15 16:30:18 +0300

    Time: 34 ms (60.65%) | Memory: 16.6 MB (26.03%) - LeetSync

---
+++ leetcode/easy/1252-break-a-palindrome/break-a-palindrome.py
@@ -0,0 +1,12 @@
+class Solution:
+    def breakPalindrome(self, p: str) -> str:
+        if(len(p) == 1):
+            return  ""
+        n = len(p)
+        p = list(p)
+        for i in range(n//2):
+            if(p[i] != 'a'):
+                p[i] = "a"
+                return "".join(p)
+        p[-1] = "b"
+        return "".join(p)
