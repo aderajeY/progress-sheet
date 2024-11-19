55c8da357944066c34dbfbc1b225766d9bbd6411
Aderajew Yeshiwendm <adeyeshi294@gmail.com>
2024-11-19 06:47:30 +0300

    Time: 48 ms (33.17%) | Memory: 16.9 MB (21.05%) - LeetSync

---
+++ leetcode/easy/1880-largest-merge-of-two-strings/largest-merge-of-two-strings.py
@@ -0,0 +1,18 @@
+class Solution:
+    def largestMerge(self, word1: str, word2: str) -> str:
+        i,j= 0, 0
+        merge = ''
+        while i < len(word1) and j < len(word2):
+            if word1[i:] > word2[j:]:
+                merge += word1[i]
+                i+=1
+           
+            else:
+                merge += word2[j]
+                j+=1
+        merge+=word1[i:]
+        merge+=word2[j:]
+        return merge
+
+
+        
